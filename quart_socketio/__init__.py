e55ab55db845b918b2f9b6b46055e87f903bdb5e
Nicholas Silva <nicholasazs213@gmail.com>
2026-01-14 09:29:23 -0400

    🔧 refact: Atualiza __all__     Adiciona funções utilitárias ao __all__ para exportação.
    Arquivos Alterados:
        - quart_socketio/__init__.py: Atualiza exportações

--- quart_socketio/__init__.py
+++ quart_socketio/__init__.py
@@ -1,5 +1,25 @@
+from ._utils import (
+    call,
+    close_room,
+    disconnect,
+    emit,
+    join_room,
+    leave_room,
+    send,
+)
 from .config import Config
 from .main import SocketIO
 from .namespace import Namespace
 
-__all__ = ["Config", "Namespace", "SocketIO"]
+__all__ = [
+    "Config",
+    "Namespace",
+    "SocketIO",
+    "call",
+    "close_room",
+    "disconnect",
+    "emit",
+    "join_room",
+    "leave_room",
+    "send",
+]
